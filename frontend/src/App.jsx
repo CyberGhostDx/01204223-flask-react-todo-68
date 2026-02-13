6b2b77e77ec26309db4ea3be9c91a76879d107f8
Pratchayanon Thainngoen <feenfrink@gmail.com>
2026-02-13 13:57:11 +0700

    feat: detach todo to components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Todo from "./components/Todo";
 import "./App.css";
 
 function App() {
@@ -103,51 +102,14 @@ function App() {
       <h1>Todo List</h1>
       <ul>
         {todoList.map((todo) => (
-          <li key={todo.id}>
-            <span className={todo.done ? "done" : ""}>{todo.title}</span>
-            <button
-              onClick={() => {
-                toggleDone(todo.id);
-              }}
-            >
-              Toggle
-            </button>
-            <button
-              onClick={() => {
-                deleteTodo(todo.id);
-              }}
-            >
-              ❌
-            </button>
-            {todo.comments && todo.comments.length > 0 && (
-              <>
-                <b>Comments:</b>
-                <ul>
-                  {todo.comments.map((comment) => (
-                    <li key={comment.id}>{comment.message}</li>
-                  ))}
-                </ul>
-              </>
-            )}
-            <div className="new-comment-forms">
-              <input
-                type="text"
-                value={newComments[todo.id] || ""}
-                onChange={(e) => {
-                  const value = e.target.value;
-                  setNewComments({ ...newComments, [todo.id]: value });
-                }}
-              />
-
-              <button
-                onClick={() => {
-                  addNewComment(todo.id);
-                }}
-              >
-                Add Comment
-              </button>
-            </div>
-          </li>
+          <Todo
+            key={todo.id}
+            todo={todo}
+            toggleDone={toggleDone}
+            deleteTodo={deleteTodo}
+            addNewComment={addNewComment}
+            newComments={newComments}
+          />
         ))}
       </ul>
       New:{" "}
---
+++ frontend/src/components/Todo.jsx
@@ -0,0 +1,51 @@
+const Todo = ({ todo, toggleDone, deleteTodo, addNewComment, newComments }) => {
+  return (
+    <li>
+      <span className={todo.done ? "done" : ""}>{todo.title}</span>
+      <button
+        onClick={() => {
+          toggleDone(todo.id);
+        }}
+      >
+        Toggle
+      </button>
+      <button
+        onClick={() => {
+          deleteTodo(todo.id);
+        }}
+      >
+        ❌
+      </button>
+      {todo.comments && todo.comments.length > 0 && (
+        <>
+          <b>Comments:</b>
+          <ul>
+            {todo.comments.map((comment) => (
+              <li key={comment.id}>{comment.message}</li>
+            ))}
+          </ul>
+        </>
+      )}
+      <div className="new-comment-forms">
+        <input
+          type="text"
+          value={newComments[todo.id] || ""}
+          onChange={(e) => {
+            const value = e.target.value;
+            setNewComments({ ...newComments, [todo.id]: value });
+          }}
+        />
+
+        <button
+          onClick={() => {
+            addNewComment(todo.id);
+          }}
+        >
+          Add Comment
+        </button>
+      </div>
+    </li>
+  );
+};
+
+export default Todo;
