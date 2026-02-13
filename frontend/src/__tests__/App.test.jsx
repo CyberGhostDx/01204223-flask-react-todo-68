136a61f6f2342db9d4ba9854a0583991d1eb5304
Pratchayanon Thainngoen <feenfrink@gmail.com>
2026-02-13 15:31:51 +0700

    feat: add test for App

--- frontend/src/__tests__/App.test.jsx
+++ frontend/src/__tests__/App.test.jsx
@@ -3,6 +3,19 @@ import { vi } from "vitest";
 import App from "../App.jsx";
 import "@testing-library/jest-dom";
 
+const todoItem1 = { id: 1, title: "First todo", done: false, comments: [] };
+const todoItem2 = {
+  id: 2,
+  title: "Second todo",
+  done: false,
+  comments: [
+    { id: 1, message: "First comment" },
+    { id: 2, message: "Second comment" },
+  ],
+};
+
+const originalTodoList = [todoItem1, todoItem2];
+
 const mockResponse = (body, ok = true) =>
   Promise.resolve({
     ok,
@@ -20,20 +33,7 @@ describe("App", () => {
   });
 
   it("renders correctly", async () => {
-    global.fetch.mockImplementationOnce(() =>
-      mockResponse([
-        { id: 1, title: "First todo", done: false, comments: [] },
-        {
-          id: 2,
-          title: "Second todo",
-          done: false,
-          comments: [
-            { id: 1, message: "First comment" },
-            { id: 2, message: "Second comment" },
-          ],
-        },
-      ]),
-    );
+    global.fetch.mockImplementationOnce(() => mockResponse(originalTodoList));
 
     render(<App />);
 
@@ -41,5 +41,23 @@ describe("App", () => {
     expect(await screen.findByText("Second todo")).toBeInTheDocument();
     expect(await screen.findByText("First comment")).toBeInTheDocument();
     expect(await screen.findByText("Second comment")).toBeInTheDocument();
+  });
+  it("toggles done on a todo item", async () => {
+    const toggledTodoItem1 = { ...todoItem1, done: true };
+
+    global.fetch
+      .mockImplementationOnce(() => mockResponse(originalTodoList))
+      .mockImplementationOnce(() => mockResponse(toggledTodoItem1));
+
+    render(<App />);
+
+    expect(await screen.findByText("First todo")).not.toHaveClass("done");
+
+    const toggleButtons = await screen.findAllByRole("button", {
+      name: /toggle/i,
+    });
+    toggleButtons[0].click();
+
+    expect(await screen.findByText("First todo")).toHaveClass("done");
   });
 });
